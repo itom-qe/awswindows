7d7d91ce7cbfd8ca1c21bb93fcfa44ff7af72bd2
itom-qe <79703359+itom-qe@users.noreply.github.com>
2023-05-19 15:41:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "region" {}
+
+variable "subnet" {}
+
+variable "network" {}
+
+variable "instancetype" {}
+
+variable "ami" {}
+
+variable "keyname" {}
+
+variable "keypath" {}
